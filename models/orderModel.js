ad2c1f39b1c90cda2b40b939bb9df0eea9e8c2b9
Sanjeev Sharma <sanjeev.sharma@keensolution.in>
2024-08-05 18:12:42 +0530

    first commit

---
+++ models/orderModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const orderSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		userid: { type: String, required: true },
+		orderItems: [],
+		shippingAddress: { type: String, required: true },
+		orderAmount: { type: Number, required: true },
+		isDelivered: { type: Boolean, default: false },
+		orderId: { type: String, required: true },
+		paymentId: { type: String, required: true },
+		orderstatus:{type: String, default:""}
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model('orders', orderSchema);
